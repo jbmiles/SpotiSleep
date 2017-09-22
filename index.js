248c8da43dd46c62590c8a79705f3596c73d6a4c
Tehmo3 <jasper.b.miles@gmail.com>
2017-09-22 23:31:44 +1000

    updating server code

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.redirect('/getAuth');
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('app listening on port 3000!');
 });
 
-app.get("/getAuth", function(req, res) {
+app.get("/getAuth", (req, res) => {
   const scopes = ["user-modify-playback-state"];
   const state = "this-some-state-yo"
   // Create the authorization URL
@@ -27,39 +27,34 @@ app.get("/getAuth", function(req, res) {
   res.redirect(authorizeURL);
 });
 
-app.get("/redirect", function(req, res) {
-  spotifyApi.authorizationCodeGrant(req.query.code)
-  .then(data => {
-    spotifyApi.setAccessToken(data.body['access_token']);
-    spotifyApi.setRefreshToken(data.body['refresh_token']);
-    setInterval(function() {
-        spotifyApi.setAccessToken(globalAccessToken);
-        spotifyApi.setRefreshToken(globalRefreshToken);
-        spotifyApi.refreshAccessToken()
-        .then(function(data) {
-          console.log('The access token has been refreshed!');
-
-          spotifyApi.setAccessToken(data.body['access_token']);
-          globalAccessToken = data.body['access_token'];
-        }, function(err) {
-          console.log('Could not refresh access token', err);
-        });
-      },1800000)
-  }, err => {
-    console.log('Something went wrong!', err);
-  });
+app.get("/redirect", (req, res) => {
+  authorizeCode(req.query.code);
   res.sendFile('./timer.html', {root: __dirname+'/public' });
 });
 
-app.post('/setTimer', function(req, res) {
+app.post('/setTimer', (req, res) => {
   const time = req.body.time;
   console.log("timer set!");
   console.log(time);
-  setTimeout(() =>{
-    spotifyApi.pause()
-    .then(data => {
-        console.log("paused!");
-    });
+  setTimeout(async () => {
+    await spotifyApi.pause()
+    console.log("paused!");
   }, time);
   res.send('timer set');
 })
+
+async function authorizeCode(code) {
+  const data = await spotifyApi.authorizationCodeGrant(code);
+  spotifyApi.setAccessToken(data.body['access_token']);
+  spotifyApi.setRefreshToken(data.body['refresh_token']);
+  setInterval(async function() {
+      spotifyApi.setAccessToken(globalAccessToken);
+      spotifyApi.setRefreshToken(globalRefreshToken);
+      const data2 = await spotifyApi.refreshAccessToken()
+      console.log('The access token has been refreshed!');
+
+      spotifyApi.setAccessToken(data.body['access_token']);
+      globalAccessToken = data.body['access_token'];
+  },1800000)
+  return;
+}
